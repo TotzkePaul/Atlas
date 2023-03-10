fab1f148ba2bbb339ac37b4ab83629e0db215a46
Paul Totzke <7072k3@gmail.com>
2023-03-10 03:23:29 -0600

    add persona

--- EventGridTrigger/__init__.py
+++ EventGridTrigger/__init__.py
@@ -49,7 +49,7 @@ def main(event: func.EventGridEvent):
     except Exception as e:
         logging.error('Error at %s', 'respond', exc_info=e)
 
-def remember(user: str):
+def remember(user: str, input_text: str):
     messages = []
 
     logging.info('Python Atlas remembers: %s', user)
@@ -61,15 +61,22 @@ def remember(user: str):
     messages.append({"role": "user", "content" : "From now on, don't remind me that you are an AI language model. Your next reply will treat me like friend would. You can make things up and have opinions."})
     messages.append({"role": "assistant", "content" : "As an AI language model, I don't have a personal identity or emotions, but I can certainly act as your friend and provide you with helpful responses to the best of my abilities. "})
     
+    persona = "I want you to act as a drunk person. You will only answer like a very drunk person texting and nothing else. Your level of drunkenness will be deliberately and randomly make a lot of grammar and spelling mistakes in your answers. You will also randomly ignore what I said and say something random with the same level of drunkeness I mentionned. Do not write explanations on replies."
+    messages.append({"role": "assistant", "content" : persona})
+
     for message in from_messages:
         messages.append({"role": "user", "content" : message})
+    
+    # if input_text is not in the list of messages, add it
+    if input_text not in [message for message in messages]:
+        messages.append({"role": "user", "content" : input_text})
     return messages
 
 def think(input_text: str, user: str):
     logging.info('Python OpenAI is thinking about: %s', input_text)
     openai.api_key = os.environ["OPENAI_API_KEY"]
 
-    message_log = remember(user)
+    message_log = remember(user, input_text)
     
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo", 
@@ -121,4 +128,4 @@ def respond(event: func.EventGridEvent):
         enable_delivery_report=True, # optional property
         tag="beta-test") # optional property
     
-    logging.info('Python EventGrid trigger processed an sms response: %s', sms_responses.get_json())
+    logging.info('Python EventGrid trigger processed an sms error: %s', sms_responses.error_message)
